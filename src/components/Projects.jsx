b9396a433329954ceb9ee2ac37b9fa6df196339c
AnhDung13 <ad137ocean@gmail.com>
2024-08-10 20:35:58 +0700

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,42 +4,42 @@ export default function Projects() {
 
   const items = [
     {
-        imgSrc: './src/img/blog1.svg',
+        imgSrc: '../img/blog1.svg',
         title: 'Mobile App Development',
         content: 'Mobile app designed to help users discover and explore local restaurants and cuisines.',
     },
     {
-        imgSrc: './src/img/blog2.svg',
+        imgSrc: '../img/blog2.svg',
         title: 'Landing Page Development',
         content: 'Promotional landing page for a fitness website Summer Campaign. Form development included.',
     },
     {
-        imgSrc: './src/img/blog3.svg',
+        imgSrc: '../img/blog3.svg',
         title: 'Mobile App Development',
         content: 'Mobile app designed to help users discover and explore local restaurants and cuisines.',
     },
     {
-        imgSrc: './src/img/blog4.svg',
+        imgSrc: '../img/blog4.svg',
         title: 'E-commerce development',
         content: 'Ecommerce website offering access to the latest and greatest gadgets and accessories.',
     },
     {
-        imgSrc: './src/img/blog1.svg',
+        imgSrc: '../img/blog1.svg',
         title: 'Mobile App Development',
         content: 'Mobile app designed to help users discover and explore local restaurants and cuisines.',
     },
     {
-        imgSrc: './src/img/blog2.svg',
+        imgSrc: '../img/blog2.svg',
         title: 'Landing Page Development',
         content: 'Promotional landing page for a fitness website Summer Campaign. Form development included.',
     },
     {
-        imgSrc: './src/img/blog3.svg',
+        imgSrc: '../img/blog3.svg',
         title: 'Mobile App Development',
         content: 'Mobile app designed to help users discover and explore local restaurants and cuisines.',
     },
     {
-        imgSrc: './src/img/blog4.svg',
+        imgSrc: '../img/blog4.svg',
         title: 'E-commerce development',
         content: 'Ecommerce website offering access to the latest and greatest gadgets and accessories.',
     },
@@ -54,8 +54,8 @@ export default function Projects() {
             </p>
         </div>
         <div className='container mx-auto grid grid-cols-1 gap-x-10 gap-y-20 md:grid-cols-2 xl:grid-cols-4'>
-            {items.map(({imgSrc, title, content})=>
-                <div className='flex flex-col text-gray-700'>
+            {items.map(({imgSrc, title, content}, index)=>
+                <div key={index} className='flex flex-col text-gray-700'>
                     <img src={imgSrc} alt="" className='h-full w-full object-cover'/>
                     <a href="#" className='text-blue-gray-900'>
                         <h3 className='text-xl font-semibold leading-snug text-inherit mb-2'>{title}</h3>
